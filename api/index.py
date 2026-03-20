b3d988544248fae86350243a1c402eef6d5d1f43
Dev Khanna <devkhanna05@gmail.com>
2026-03-20 20:56:14 +0530

    index.py updated

--- api/index.py
+++ api/index.py
@@ -5,4 +5,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app
 
+# Fix template path
+app.template_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'templates')
+
 handler = app
